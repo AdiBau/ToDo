5fa9186ea500292198fe32af3610a06e47139937
AdiBau <99968312+AdiBau@users.noreply.github.com>
2023-02-09 13:46:44 +0100

    Rename file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = require('./router/router')
 const path = require('path')
 
-const notFound = require('./controlers/notfound')
+const notFound = require('./controlers/notFound')
 
 const app = express()
 
